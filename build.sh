d6e26301fafc570dfef8e69c3968a56415f5782d
Marian Marinov <mm@yuhu.biz>
2014-07-28 19:01:13 +0300

    build.sh: Script for building the archive out of the current dir

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd ~/Projects/perl-containers/
+
+proj='Linux-Unshare'
+ver=$(awk '/^version:/{print $2}' $proj/META.yml)
+excludes='Makefile.old .git build.sh'
+
+if [ -z "$ver" ]; then
+	echo "Unable to get $poj version"
+	exit 1
+else
+	echo "Building project $proj version $ver"
+fi
+
+
+if [ ! -d $proj ]; then
+	echo "Unable to find $proj dir"
+	exit 2
+fi
+
+for i in $excludes; do
+	exclude_option="$exclude_option --exclude $i"
+done
+
+if [ -f $proj-${ver}.tar.gz ]; then
+	rm -f $proj-${ver}.tar.gz
+fi
+
+mv $proj ${proj}-$ver
+tar $exclude_option -czf $proj-${ver}.tar.gz ${proj}-$ver
+mv ${proj}-$ver $proj
+
