6e9d861d9c12fe36808c606821f79e788e32d676
Patrice Clement <monsieurp@gentoo.org>
2017-03-11 22:20:16 +0100

    use [[ operator instead of standard [ (test) operator.
    This is a bash-ism but hey, we're dealing with bash so we can. :-)

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ proj='Linux-Unshare'
 ver=$(awk '/^version:/{print $2}' $proj/META.yml)
 excludes=( Makefile.old .git build.sh up-version.sh )
 
-if [ -z "$ver" ]; then
+if [[ -z "$ver" ]]; then
 	echo "Unable to get $proj version"
 	exit 1
 fi
 
 echo "Building project $proj version $ver"
 
-if [ ! -d "$proj" ]; then
+if [[ ! -d "$proj" ]]; then
 	echo "Unable to find $proj dir"
 	exit 2
 fi
@@ -21,7 +21,7 @@ for i in "${excludes[@]}"; do
 	exclude_option="$exclude_option --exclude $i"
 done
 
-if [ -f $proj-${ver}.tar.gz ]; then
+if [[ -f $proj-${ver}.tar.gz ]]; then
 	rm -f "$proj-${ver}.tar.gz"
 fi
 
